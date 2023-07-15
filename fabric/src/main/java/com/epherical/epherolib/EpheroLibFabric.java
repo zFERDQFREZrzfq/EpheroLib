b8c006a9d03638c7c987bcf6dbeb239c84e5f167
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2023-07-15 10:04:08 -0500

    add a method for networking in root only

--- fabric/src/main/java/com/epherical/epherolib/EpheroLibFabric.java
+++ fabric/src/main/java/com/epherical/epherolib/EpheroLibFabric.java
@@ -1,6 +1,5 @@
 package com.epherical.epherolib;
 
-import com.epherical.epherolib.config.LanguageConfig;
 import net.fabricmc.api.ModInitializer;
 
 public class EpheroLibFabric implements ModInitializer {
@@ -9,8 +8,6 @@ public class EpheroLibFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         CommonPlatform.create(new FabricPlatform());
-        LanguageConfig config = new LanguageConfig("translations");
-        config.loadTranslations(ModConstants.MOD_ID, "en_us");
 
     }
 }
